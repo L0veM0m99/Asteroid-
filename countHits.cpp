34ea21012f556b5a7643e2da3ab217d30a546812
Nanthana Thanonklin <nthanonklin@gmail.com>
2021-09-27 23:05:26 -0700

    adding countHit.cpp

---
+++ countHits.cpp
@@ -0,0 +1,21 @@
+
+//Nanthana Thanonklin
+
+//countHits.cpp contains the implementation of countsHits function
+#include "asteroid.h"
+
+#include <iostream>
+#include  <vector>
+using namespace std;
+//#endif
+
+int countHits(std::vector<Asteroid> a){
+
+	int hits = 0;
+
+    //vector<Asteroid> currentA = a[0];
+
+
+    return hits;
+
+}

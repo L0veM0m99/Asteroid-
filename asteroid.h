80dd0ddf16f81b92254c25ca18329cb72164fd51
Nanthana Thanonklin <nthanonklin@gmail.com>
2021-09-27 23:06:56 -0700

    Asteroid files

---
+++ asteroid.h
@@ -0,0 +1,14 @@
+
+//Nanthana Thanonklin
+//asteroid.h
+
+
+enum Direction{left, right}; // by default left = 0, right =1
+
+//Asteroid structure specified mass and direction
+struct Asteroid {
+        int m; // Mass
+        Direction d; //Direction
+};
+
+
---
+++ countHits.h
@@ -0,0 +1,17 @@
+
+//Nanthana Thanonklin
+//countHits.h
+
+// countHits.h includes the prototype of countHits function so we 
+// can include from our main program, 
+// includes asteroid.h so we can use our definition(Asteroid)
+
+#ifndef asteroid_h
+#define asteroid_h
+#endif
+#include  <vector>
+
+int countHits(std::vector<Asteroid> a);
+        
+
+
